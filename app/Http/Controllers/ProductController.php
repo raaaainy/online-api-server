d91518eec5792214ac46e2c1461375a5e151bf08
Clarissa Barr <clarissab8788@gmail.com>
2026-02-11 22:12:02 -0800

    'yes'

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'rating' => 'required',
+        ]);
+
+        $placeholder = "https://placehold.co/600x400";
+        $product = Product::create([
+            'name'  => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'rating' => $request->rating,
+            'image' => $placeholder,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $product = Product::find($id);
+
+        $placeholder = "https://placehold.co/600x400";
+
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'rating' => 'required',
+        ]);
+
+        $product->update([
+            'name'  => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'rating' => $request->rating,
+            'image' => $placeholder,
+        ]);
+        
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+    }
+}
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $placeholder = "https://placehold.co/600x400";
+        return [
+            'name' => $this->faker->words(2, true),
+            'description' => $this->faker->sentence(),
+            'price' => $this->faker->randomFloat(2, 10, 100),
+            'rating' => $this->faker->numberBetween(1, 5),
+            'image' => $placeholder,
+        ];
+    }
+}
